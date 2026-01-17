47b076a77d18affe3c94b32d86cb7c84a408d23c
jithinmathws <jithinmathws@gmail.com>
2026-01-17 17:56:35 +0530

    Split settings
    For easy management

---
+++ backend_django/config/settings/local.py
@@ -0,0 +1,25 @@
+from os import getenv, path
+from dotenv import load_dotenv
+from .base import * # noqa
+from .base import BASE_DIR
+
+local_env_file = path.join(BASE_DIR, ".envs", ".env.local")
+
+if path.exists(local_env_file):
+    load_dotenv(local_env_file)
+else:
+    raise FileNotFoundError("Local environment file not found")
+
+SECRET_KEY = getenv("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = getenv("DEBUG", default="False").lower() in ("true", "1", "t")
+
+SITE_NAME = getenv("SITE_NAME", "Local")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+DOMAIN = getenv("DOMAIN")
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
